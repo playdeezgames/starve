5f7a7e881010f72dabc37a39e04ee82969dd203c
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-01 04:25:34 -0500

    hunger rate

--- src/Starve.Business/CharacterExtensions.vb
+++ src/Starve.Business/CharacterExtensions.vb
@@ -17,6 +17,7 @@ Public Module CharacterExtensions
         If nextCell Is Nothing OrElse Not nextCell.IsTenable Then
             Return
         End If
+        character.ApplyHunger()
         character.Cell = nextCell
         currentCell.Character = Nothing
         nextCell.Character = character
@@ -26,6 +27,10 @@ Public Module CharacterExtensions
         Return character.Statistic(StatisticTypes.Health)
     End Function
     <Extension>
+    Public Sub SetHealth(character As ICharacter, health As Integer)
+        character.Statistic(StatisticTypes.Health) = Math.Clamp(health, 0, character.MaximumHealth)
+    End Sub
+    <Extension>
     Public Function MaximumHealth(character As ICharacter) As Integer
         Return character.Statistic(StatisticTypes.MaximumHealth)
     End Function
@@ -34,7 +39,23 @@ Public Module CharacterExtensions
         Return character.Statistic(StatisticTypes.Satiety)
     End Function
     <Extension>
+    Public Sub SetSatiety(character As ICharacter, satiety As Integer)
+        character.Statistic(StatisticTypes.Satiety) = Math.Clamp(satiety, 0, character.MaximumSatiety)
+    End Sub
+    <Extension>
     Public Function MaximumSatiety(character As ICharacter) As Integer
         Return character.Statistic(StatisticTypes.MaximumSatiety)
     End Function
+    <Extension>
+    Public Function HungerRate(character As ICharacter) As Integer
+        Return character.Statistic(StatisticTypes.HungerRate)
+    End Function
+    <Extension>
+    Public Sub ApplyHunger(character As ICharacter)
+        Dim hungerRate = character.HungerRate
+        Dim satiety = character.Satiety
+        character.SetSatiety(character.Satiety - hungerRate)
+        hungerRate = Math.Max(0, hungerRate - satiety)
+        character.SetHealth(character.Health - hungerRate)
+    End Sub
 End Module
