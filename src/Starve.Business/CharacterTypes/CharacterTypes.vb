cc90fd1db4bdfdc4d26f1b3eb2760496c1960f84
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-01 05:07:06 -0500

    snek is character type

--- src/Starve.Business/CharacterTypes/CharacterTypes.vb
+++ src/Starve.Business/CharacterTypes/CharacterTypes.vb
@@ -2,6 +2,7 @@
 
 Friend Module CharacterTypes
     Friend Const Dude = "Dude"
+    Friend Const Snek = "Snek"
     Private ReadOnly descriptors As IReadOnlyDictionary(Of String, CharacterTypeDescriptor) =
         New Dictionary(Of String, CharacterTypeDescriptor) From
         {
@@ -9,7 +10,8 @@ Friend Module CharacterTypes
                 Dude,
                 New CharacterTypeDescriptor(
                     "$"c,
-                    Hue.Brown, statistics:=New Dictionary(Of String, Integer) From
+                    Hue.Brown,
+                    statistics:=New Dictionary(Of String, Integer) From
                     {
                         {StatisticTypes.Health, 100},
                         {StatisticTypes.MaximumHealth, 100},
@@ -17,7 +19,23 @@ Friend Module CharacterTypes
                         {StatisticTypes.MaximumSatiety, 100},
                         {StatisticTypes.HungerRate, 1},
                         {StatisticTypes.MovesMade, 0}
-                    })}
+                    })
+            },
+            {
+                Snek,
+                New CharacterTypeDescriptor(
+                    "2"c,
+                    Hue.LightGreen,
+                    statistics:=New Dictionary(Of String, Integer) From
+                    {
+                        {StatisticTypes.Health, 10},
+                        {StatisticTypes.MaximumHealth, 10},
+                        {StatisticTypes.Satiety, 0},
+                        {StatisticTypes.MaximumSatiety, 0},
+                        {StatisticTypes.HungerRate, 0},
+                        {StatisticTypes.MovesMade, 0}
+                    })
+            }
         }
     <Extension>
     Friend Function ToCharacterTypeDescriptor(characterType As String) As CharacterTypeDescriptor
