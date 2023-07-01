36858f8f4cf50fff45767e9d787e36880d549fbb
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-01 03:58:02 -0500

    folders!

---
+++ src/Starve.Business/StatisticTypes.vb
@@ -0,0 +1,7 @@
+﻿Friend Module StatisticTypes
+    Friend Const Satiety = "Satiety"
+    Friend Const MaximumSatiety = "MaximumSatiety"
+    Friend Const Health = "Health"
+    Friend Const MaximumHealth = "MaximumHealth"
+    Friend Const HungerRate = "HungerRate"
+End Module
