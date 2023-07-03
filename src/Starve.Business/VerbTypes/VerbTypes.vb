14c5f1bda6125f8f88bfad05daffe2e01cd22f80
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-03 04:35:58 -0500

    verbs exist

---
+++ src/Starve.Business/VerbTypes/VerbTypes.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Runtime.CompilerServices
+
+Friend Module VerbTypes
+    Friend Const Eat = "Eat"
+    Private ReadOnly descriptors As IReadOnlyDictionary(Of String, VerbDescriptor) =
+        New Dictionary(Of String, VerbDescriptor) From
+        {
+            {
+                Eat,
+                New VerbDescriptor("Eat")
+            }
+        }
+    <Extension>
+    Friend Function ToVerbTypeDescriptor(verbType As String) As VerbDescriptor
+        Return descriptors(verbType)
+    End Function
+    Friend ReadOnly Property All As IEnumerable(Of String)
+        Get
+            Return descriptors.Keys
+        End Get
+    End Property
+End Module
