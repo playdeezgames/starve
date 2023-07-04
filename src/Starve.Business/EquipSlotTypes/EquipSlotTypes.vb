aab7714fa9ee360a2e299665f549d47b27455e1a
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-04 04:25:08 -0500

    equip slot types

---
+++ src/Starve.Business/EquipSlotTypes/EquipSlotTypes.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Runtime.CompilerServices
+
+Friend Module EquipSlotTypes
+    Friend Const Weapon = "Weapon"
+    Private ReadOnly descriptors As IReadOnlyDictionary(Of String, EquipSlotTypeDescriptor) =
+        New Dictionary(Of String, EquipSlotTypeDescriptor) From
+        {
+            {Weapon, New EquipSlotTypeDescriptor("Weapon")}
+        }
+    <Extension>
+    Friend Function ToEquipSlotTypeDescriptor(equipSlotType As String) As EquipSlotTypeDescriptor
+        Return descriptors(equipSlotType)
+    End Function
+    Friend ReadOnly Property All As IEnumerable(Of String)
+        Get
+            Return descriptors.Keys
+        End Get
+    End Property
+End Module
