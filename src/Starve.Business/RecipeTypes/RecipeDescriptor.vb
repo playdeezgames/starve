3a09a03d7a40d8b24799a73d9b07e131315beb6d
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-07-05 05:07:40 -0500

    recipe descriptors exist

---
+++ src/Starve.Business/RecipeTypes/RecipeDescriptor.vb
@@ -0,0 +1,8 @@
+﻿Friend Class RecipeDescriptor
+    Friend ReadOnly Property Inputs As IReadOnlyDictionary(Of String, Integer)
+    Friend ReadOnly Property Outputs As IReadOnlyDictionary(Of String, Integer)
+    Sub New(inputs As IReadOnlyDictionary(Of String, Integer), outputs As IReadOnlyDictionary(Of String, Integer))
+        Me.Inputs = inputs
+        Me.Outputs = outputs
+    End Sub
+End Class
---
+++ src/Starve.Business/RecipeTypes/RecipeTypes.vb
@@ -0,0 +1,16 @@
+﻿Friend Module RecipeTypes
+    Friend ReadOnly Descriptors As IReadOnlyList(Of RecipeDescriptor) =
+        New List(Of RecipeDescriptor) From
+        {
+            New RecipeDescriptor(
+                New Dictionary(Of String, Integer) From
+                {
+                    {ItemTypes.Rock, 2}
+                },
+                New Dictionary(Of String, Integer) From
+                {
+                    {ItemTypes.Rock, 1},
+                    {ItemTypes.SharpRock, 1}
+                })
+        }
+End Module
